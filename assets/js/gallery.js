0a110e074f5e76bd078a50690b7defcce137d690
Jeff Cela <cela.j@labrador-company.com>
2021-08-26 11:30:32 -0400

    adjusted poistion of the close action for dev sheet modal

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -50,3 +50,26 @@ function preview(element){
     document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
   }
 }
+
+
+// Back to top navigation button
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
